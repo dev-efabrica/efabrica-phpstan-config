88b1ab6cf2cb6e1078dfbe544e5e763559bfd1c9
Michal Lulco <lulco@efabrica.sk>
2023-01-07 23:08:05 +0100

    Updated composer, added src to autoload

---
+++ src/LatteTemplateResolver/NotCmsPluginLatteTemplateResolver.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Efabrica\PhpstanConfig\LatteTemplateResolver;
+
+final class NotCmsPluginLatteTemplateResolver implements CustomLatteTemplateResolverInterface
+{
+
+}
