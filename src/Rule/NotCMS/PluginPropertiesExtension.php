0c9519fea9a314de825b0eabc758ffe6a542f355
Samuel Kelemen <kelemen.samuel@gmail.com>
2023-07-29 12:48:22 +0200

    NotCMS plugin properties

---
+++ src/Rule/NotCMS/PluginPropertiesExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPStanConfig\Rule\NotCMS;
+
+use PHPStan\Reflection\PropertyReflection;
+use PHPStan\Rules\Properties\ReadWritePropertiesExtension;
+use function strpos;
+
+class PluginPropertiesExtension implements ReadWritePropertiesExtension
+{
+    public function isAlwaysRead(PropertyReflection $property, string $propertyName): bool
+    {
+        return false;
+    }
+
+    public function isAlwaysWritten(PropertyReflection $property, string $propertyName): bool
+    {
+        return $this->isInitialized($property, $propertyName);
+    }
+
+    public function isInitialized(PropertyReflection $property, string $propertyName): bool
+    {
+        return $property->isPublic() &&
+            strpos($property->getDocComment() ?? '', '@plugin-initialized') !== false;
+    }
+
+}
